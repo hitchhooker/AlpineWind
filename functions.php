5cc592b95986937a44d18f11c289ead652d240f5
hitchhooker <tommi@niemi.lol>
2021-11-29 21:12:12 +0000

    update gitignore

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /**
  * In order to avoid cluttering your functions file, we include our file here at
- * the top. It is responsible for enqueuing our styles (such as Tailwind CSS),
+ * the top. It is responsible for enqueuing our styles (such as Windi CSS),
  * script files (including Alpine), and enabling Blade templating support.
  */
 require_once __DIR__ . '/alpinewind/functions.php';
