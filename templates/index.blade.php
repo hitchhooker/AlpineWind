92fe4e44c4d06403880b09c620bcd68c7d6d22a0
hitchhooker <tommi@niemi.lol>
2021-12-01 00:32:11 +0000

    add stricter settings and change example

--- templates/index.blade.php
+++ templates/index.blade.php
@@ -1,9 +1,13 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="flex items-center h-screen bg-blue-900 bg-hero" x-bind="{ changeBg: true }" :class="{ 'bg-example' : !changeBg , 'bg-hero' : changeBg}">
-        <div class="max-w-5xl m-auto bg-white rounded-lg w-3/4 h-64 flex items-center shadow-xl" x-data="{ showPhrase: true }" @mouseover="changeBg = true" @mouseout="changeBg = false">
-            <h1 class="w-full text-center text-3xl md:text-5xl font-mono" @mouseover="showPhrase = false" @mouseout="showPhrase = true" x-text="showPhrase ? 'Build Something Amazing...' : '...With Alpine Wind'"></h1>
+<div x-data="{ bgHero: false }">
+    <!-- TODO: make binding work -->
+    <div class="flex items-center h-screen bg-blue-900" x-bind:class="! bgHero ? 'bg-example' : 'bg-hero'">
+        <div class="max-w-5xl m-auto bg-pink-200 rounded-lg w-3/4 h-64 flex items-center shadow-xl" x-data="{ open: false }">
+        <button class="border-1 px-8 py-4 bg-white rounded-lg shadow-lg" @click="open = ! open">Toggle</button>
+            <h1 class="w-full text-center text-3xl md:text-5xl font-mono" x-show="open">
+           		AlpineWind
+            </h1>
+            <div class="bg-hero bg-example"></div>
+            <button class="border-3 px-10 py-5 bg-black text-white rounded-lg shadow-lg" @click="bgHero = !bgHero">Background change</button>
         </div>
     </div>
-@endsection
+</div>
