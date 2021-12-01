bc08448cb559e6840c3519b51a972b7d9e0c93b7
hitchhooker <tommi@niemi.lol>
2021-12-01 01:06:18 +0000

    add brotli

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require("laravel-mix")
-require("laravel-mix-webp");
+require("laravel-mix-webp")
+require("laravel-mix-brotli");
 
 mix.js(`src/scripts/app.js`, "dist/")
     .options({
@@ -11,5 +12,12 @@ mix.js(`src/scripts/app.js`, "dist/")
         from: 'src/images',
         to: 'dist/images'
 	})
+	.brotli({
+        enabled: mix.inProduction(),
+		asset: '[path].br[query]',
+		test: /\.(js|css|html|svg)$/,
+		threshold: 10240,
+		minRatio: 0.8
+	});
     .setPublicPath('dist');
     
